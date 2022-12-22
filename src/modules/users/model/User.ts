69e35b21c0468714e1f53c5f2d8d3635c96e9a0d
gabrielodeveloper <gabriel0211oliveira15@gmail.com>
2022-12-22 15:45:41 -0300

    backend challenge nodejs

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,16 +1,11 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  id?: string;
-
+  id: string;
   name: string;
-
   admin: boolean;
-
   email: string;
-
   created_at: Date;
-
   updated_at: Date;
 
   constructor() {
